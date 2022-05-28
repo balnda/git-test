6428fc8cb29bf6583dc774a72eedaf85543da522
Balajee Raghbendra <balajeeraghbendra@gmail.com>
2022-05-28 20:05:24 +1000

    changed modulus for testing rebase

--- modulus.js
+++ modulus.js
@@ -1,5 +1,6 @@
 // modulus.js
 // return remainder of two numbers
-function modulus(a, b) {
-  return a % b;
+// return remainder of two numbers
+function modulus(a, b, bMODULUSa) {
+  return (bMODULUSa) ?  b % a : a % b;
 };
